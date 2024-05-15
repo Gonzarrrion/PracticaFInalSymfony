2fd6bbb58d33e67025a197ad328feb928c064ff0
Javipral7 <jprietoal.inf@upsa.es>
2024-05-15 02:34:19 +0200

    se pueden editar libros por id

--- ProyectoSymfony/var/cache/dev/url_matching_routes.php
+++ ProyectoSymfony/var/cache/dev/url_matching_routes.php
@@ -12,11 +12,13 @@ return [
     [ // $regexpList
         0 => '{^(?'
                 .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+                .'|/editar/libro/([^/]++)(*:64)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
-        35 => [
-            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+        35 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        64 => [
+            [['_route' => 'app_editar_libro', '_controller' => 'App\\Controller\\EditarLibroController::index'], ['id'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
