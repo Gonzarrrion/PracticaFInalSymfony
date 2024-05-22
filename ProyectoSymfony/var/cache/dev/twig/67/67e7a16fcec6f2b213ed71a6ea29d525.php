17df1b5c4a4225d1660a2001b9e6f08ee369a278
Javipral7 <jprietoal.inf@upsa.es>
2024-05-22 18:52:45 +0200

    editar

--- ProyectoSymfony/var/cache/dev/twig/67/67e7a16fcec6f2b213ed71a6ea29d525.php
+++ ProyectoSymfony/var/cache/dev/twig/67/67e7a16fcec6f2b213ed71a6ea29d525.php
@@ -73,67 +73,46 @@ class __TwigTemplate_a4906af284c37a204eef512c6ef7672d extends Template
 
         // line 6
         yield "<div class=\"container\">
-<h1>Funcionalidades</h1>
-";
+    <h1>Funcionalidades</h1>
+    ";
         // line 8
         $context['_parent'] = $context;
         $context['_seq'] = CoreExtension::ensureTraversable(CoreExtension::getAttribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 8, $this->source); })()), "flashes", ["success"], "method", false, false, false, 8));
         foreach ($context['_seq'] as $context["_key"] => $context["message"]) {
             // line 9
-            yield "            <div class=\"alert alert-success\">";
+            yield "        <div class=\"alert alert-success\">";
             yield Twig\Extension\EscaperExtension::escape($this->env, $context["message"], "html", null, true);
             yield "</div>
-";
+    ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['message'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
         // line 11
-        yield "<div>
-    <a href=\"";
+        yield "    <div>
+        <a href=\"";
         // line 12
         yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("anadir_nuevo_libro");
         yield "\" class=\"btn btn-success\">Añadir nuevo Libro</a>
-    <a href=\"";
+        <a href=\"";
         // line 13
         yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("insertar_biblioteca");
         yield "\" class=\"btn btn-success\">Añadir nueva Biblioteca</a>
-</div>
+    </div>
 
+    <h2>Editar Libro</h2>
+    <p>Para editar un libro, dirigete a la URL: <code>";
+        // line 17
+        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("editar_libro", ["id" => "ID_DEL_LIBRO"]);
+        yield "</code></p>
+    <p>Reemplaza <code>ID_DEL_LIBRO</code> con el ID del libro que deseas editar.</p>
 
-
-
-<h2>Editar Libro</h2>
-<form class=\"form\" action=\"";
-        // line 20
-        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("editar_libro", ["id" => "placeholder"]);
-        yield "\" method=\"GET\" onsubmit=\"return setAction(this, 'libro')\">
-    <label class=\"labels\" for=\"libroId\">ID del Libro:</label>
-    <input class=\"inputs\" type=\"text\" id=\"libroId\" name=\"id\" required>
-    <button type=\"submit\" class=\"btn btn-primary\">Editar Libro</button>
-</form>
-
-
-
-<h2>Editar Biblioteca</h2>
-<form class=\"form\" action=\"";
-        // line 29
-        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("editar_biblioteca", ["id" => "placeholder"]);
-        yield "\" method=\"GET\" onsubmit=\"return setAction(this, 'biblioteca')\">
-    <label class=\"labels\" for=\"bibliotecaId\">ID de la Biblioteca:</label>
-    <input class=\"inputs\" type=\"text\" id=\"bibliotecaId\" name=\"id\" required>
-    <button type=\"submit\" class=\"btn btn-primary\">Editar Biblioteca</button>
-</form>
-
-";
-        // line 36
-        yield "<script>
-function setAction(form, type) {
-    const id = form.querySelector('input[name=\"id\"]').value;
-    form.action = form.action.replace('placeholder', id);
-    return true;
-}
-</script>
+    <h2>Editar Biblioteca</h2>
+    <p>Para editar una biblioteca, dirigete a la URL: <code>";
+        // line 21
+        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("editar_biblioteca", ["id" => "ID_DE_LA_BIBLIOTECA"]);
+        yield "</code></p>
+    <p>Reemplaza <code>ID_DE_LA_BIBLIOTECA</code> con el ID de la biblioteca que deseas editar.</p>
 
 </div>
 ";
@@ -164,7 +143,7 @@ function setAction(form, type) {
      */
     public function getDebugInfo()
     {
-        return array (  130 => 36,  121 => 29,  109 => 20,  99 => 13,  95 => 12,  92 => 11,  83 => 9,  79 => 8,  75 => 6,  68 => 5,  54 => 3,  37 => 1,);
+        return array (  113 => 21,  106 => 17,  99 => 13,  95 => 12,  92 => 11,  83 => 9,  79 => 8,  75 => 6,  68 => 5,  54 => 3,  37 => 1,);
     }
 
     public function getSourceContext()
@@ -175,42 +154,22 @@ function setAction(form, type) {
 
 {% block body %}
 <div class=\"container\">
-<h1>Funcionalidades</h1>
-{% for message in app.flashes('success') %}
-            <div class=\"alert alert-success\">{{ message }}</div>
-{% endfor %}
-<div>
-    <a href=\"{{ path('anadir_nuevo_libro') }}\" class=\"btn btn-success\">Añadir nuevo Libro</a>
-    <a href=\"{{ path('insertar_biblioteca') }}\" class=\"btn btn-success\">Añadir nueva Biblioteca</a>
-</div>
+    <h1>Funcionalidades</h1>
+    {% for message in app.flashes('success') %}
+        <div class=\"alert alert-success\">{{ message }}</div>
+    {% endfor %}
+    <div>
+        <a href=\"{{ path('anadir_nuevo_libro') }}\" class=\"btn btn-success\">Añadir nuevo Libro</a>
+        <a href=\"{{ path('insertar_biblioteca') }}\" class=\"btn btn-success\">Añadir nueva Biblioteca</a>
+    </div>
 
+    <h2>Editar Libro</h2>
+    <p>Para editar un libro, dirigete a la URL: <code>{{ path('editar_libro', {'id': 'ID_DEL_LIBRO'}) }}</code></p>
+    <p>Reemplaza <code>ID_DEL_LIBRO</code> con el ID del libro que deseas editar.</p>
 
-
-
-<h2>Editar Libro</h2>
-<form class=\"form\" action=\"{{ path('editar_libro', {'id': 'placeholder'}) }}\" method=\"GET\" onsubmit=\"return setAction(this, 'libro')\">
-    <label class=\"labels\" for=\"libroId\">ID del Libro:</label>
-    <input class=\"inputs\" type=\"text\" id=\"libroId\" name=\"id\" required>
-    <button type=\"submit\" class=\"btn btn-primary\">Editar Libro</button>
-</form>
-
-
-
-<h2>Editar Biblioteca</h2>
-<form class=\"form\" action=\"{{ path('editar_biblioteca', {'id': 'placeholder'}) }}\" method=\"GET\" onsubmit=\"return setAction(this, 'biblioteca')\">
-    <label class=\"labels\" for=\"bibliotecaId\">ID de la Biblioteca:</label>
-    <input class=\"inputs\" type=\"text\" id=\"bibliotecaId\" name=\"id\" required>
-    <button type=\"submit\" class=\"btn btn-primary\">Editar Biblioteca</button>
-</form>
-
-{# para añadir el id que se buscara #}
-<script>
-function setAction(form, type) {
-    const id = form.querySelector('input[name=\"id\"]').value;
-    form.action = form.action.replace('placeholder', id);
-    return true;
-}
-</script>
+    <h2>Editar Biblioteca</h2>
+    <p>Para editar una biblioteca, dirigete a la URL: <code>{{ path('editar_biblioteca', {'id': 'ID_DE_LA_BIBLIOTECA'}) }}</code></p>
+    <p>Reemplaza <code>ID_DE_LA_BIBLIOTECA</code> con el ID de la biblioteca que deseas editar.</p>
 
 </div>
 {% endblock %}
